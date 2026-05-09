7d8d085e6c0f667002d170921f1787e3505b858e
satish <satishkuncham10@gmail.com>
2026-05-09 15:01:32 +0530

    modified

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,3 +6,20 @@ public class HelloWorld {
 
     }
 }
+
+import java.util.Scanner;
+
+public class UserInput {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.println("Hello " + name);
+
+        sc.close();
+    }
+}
