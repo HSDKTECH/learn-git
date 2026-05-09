f288a34f901edbf8e078149a7625da07d7b183f7
satish <satishkuncham10@gmail.com>
2026-05-09 15:03:17 +0530

    modified

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        System.out.println("Hello,World!");
 
     }
 }
@@ -15,10 +15,11 @@ public class UserInput {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter your name: ");
+        System.out.print("Enter your sur name: ");
         String name = sc.nextLine();
 
-        System.out.println("Hello " + name);
+        System.out.println("Hello Hi" + name);
+
 
         sc.close();
     }
